f1e34b08e49900dda46ebc1547a8f59bfa3154b2
MomoNagi <lemanhhungtitdepzai@gmail.com>
2026-04-06 17:17:19 -0400

    ci: add pkgdown workflow and render script

---
+++ render.R
@@ -0,0 +1,11 @@
+pkgs <- c("pkgdown", "mlr3", "mlr3learners", "mlr3spatiotempcv", "SpatialML", "kknn", "glmnet", "rmarkdown", "knitr")
+ins.mat <- installed.packages()
+missing.pkgs <- setdiff(pkgs, rownames(ins.mat))
+install.packages(missing.pkgs)
+
+out <- capture.output(te <- try(pkgdown::build_site()))
+print(te)
+cat(out, sep="\n")
+cat(out, sep="\n", file="index_file.log")
+
+if(inherits(te, "try-error")) stop("Error during pkgdown site generation")
